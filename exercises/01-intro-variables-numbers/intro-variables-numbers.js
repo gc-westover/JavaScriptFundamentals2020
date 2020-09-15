16ff9b096990753fb3b7e7e9d3830f7f8570c97a
Greg W <gcwestover@gmail.com>
2020-09-15 17:01:11 -0400

    Solution to Excercise 1

--- exercises/01-intro-variables-numbers/intro-variables-numbers.js
+++ exercises/01-intro-variables-numbers/intro-variables-numbers.js
@@ -1,11 +1,11 @@
 function myName() {
-  // Create a variable called myName
+  let myName = 'test';
   return myName;
 }
 
 function setTeachersNames(teachersName) {
   // change code below this line so that we can set the variable.
-  const teachers = null;
+  let teachers = null;
   teachers = teachersName; //  <- this is invalid because teachers is a const. How can we fix this?
 
   return teachers;
@@ -13,25 +13,24 @@ function setTeachersNames(teachersName) {
 
 /*** return the sum of both arguments  ***/
 function add(a, b) {
-  let sum;
+  let sum = a + b;
 
   return sum;
 }
 
 /*** Using the increment operator, add a year to the argument and make me older */
 function ageMeByAYear(age) {
-  return age;
+  return ++age;
 }
 
 /*** Using the decrement operator make me age decrement by a year  ***/
 function makeMeYoungerByAYear(age) {
-  return age;
+  return --age;
 }
 
 /** Take the argument that will be a string and return an integer  */
 function convertStringToInt(str) {
-  let int;
-  return int;
+  return parseInt(str);
 }
 
 /*** Using the String method indexOf find the first position of the letter in the name  */
@@ -44,16 +43,14 @@ function convertStringToInt(str) {
  *
  */
 function getTheCharacterPosition(name, letter) {
-  let characterPosition;
+  let characterPosition = name.indexOf(letter);
   return characterPosition;
 }
 
 /** Take the argument and return the length of the argument */
 
 function stringLength(str) {
-  let strLength;
-
-  return strLength;
+  return str.length;
 }
 
 /***  return the last character of the argument
@@ -64,9 +61,7 @@ function stringLength(str) {
  */
 
 function getLastCharacter(str) {
-  let lastCharacter;
-
-  return lastCharacter;
+  return str[str.length-1];
 }
 
 /** Return the last part of a place name
@@ -82,9 +77,7 @@ function getLastCharacter(str) {
  */
 
 function getLastWordInPlaceName(place) {
-  let newPlace;
-
-  return newPlace;
+  return place.split(" ")[1];
 }
 
 /***
@@ -93,9 +86,10 @@ function getLastWordInPlaceName(place) {
  */
 
 function getLargerNumber(arg1, arg2) {
-  let largestNumber;
-
-  return largestNumber;
+  if (arg1>arg2) {
+    return arg1;
+  }
+  return arg2;
 }
 
 /*** A person just got married and they need their last name replaced.
@@ -111,7 +105,7 @@ function getLargerNumber(arg1, arg2) {
  */
 
 function replaceLastName(fullName, newLastName) {
-  let newFullName;
+  let newFullName = `${fullName.split(" ")[0]} ${newLastName}`;
   return newFullName;
 }
 
@@ -121,9 +115,9 @@ function replaceLastName(fullName, newLastName) {
  */
 
 function capitalizeLastName(fullName) {
-  let capitalizeLastName;
-
-  return capitalizeLastName;
+  let sep = fullName.split(" ");
+  let newName = sep[0] + " " + sep[1][0].toUpperCase() + sep[1].slice(1, fullName.length);
+  return newName;
 }
 
 /**

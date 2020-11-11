22622f78c5699f9c83e17e495d0e90efe579c1ff
Greg W <gcwestover@gmail.com>
2020-11-11 18:53:52 -0500

    Solutions for Async/Await

--- exercises/23-async-await/async-await.js
+++ exercises/23-async-await/async-await.js
@@ -7,3 +7,15 @@
  * This is the API you will be using. The method will be GET.
  * https://ron-swanson-quotes.herokuapp.com/v2/quotes
  */
+const blockquote = document.querySelector("#quote")
+const getQuoteBtn = document.querySelector("#getQuote")
+
+async function getQuote() {
+    const response = await fetch("https://ron-swanson-quotes.herokuapp.com/v2/quotes")
+    const quote = await response.json()
+    return quote[0]
+}
+
+getQuoteBtn.addEventListener('click', (event) => {
+    getQuote().then(quote => {blockquote.textContent = quote})
+})
